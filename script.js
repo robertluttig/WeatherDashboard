f7e9f3513225e21dbd1c9e6b700c8ee9bcb34c9f
Robert Luttig <robert.luttig@gmail.com>
2020-06-24 20:06:02 -0700

    added 5 day forecast function

--- script.js
+++ script.js
@@ -26,11 +26,55 @@ $(function() {
             $(".tempk").text("Temperature (K) " + response.main.temp);
             $(".temp").text("Temperature (F) " + tempF.toFixed(2));
             
+            fivedayForecast();
         });
     });
 });
 
+$(function () {
+    // listen for clicks on search button
+    $("#searchBtn").on("click", handleSearch);
+  
+    function handleSearch(event) {
+      event.preventDefault();
+      var search = $("#search").val();
+  
+      var APIKey = "e811c78dba75cfd7ce373e0d9345ec47";
+  
+      // Here we are building the URL we need to query the database
+      var queryURL =
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        search +
+        "&appid=" +
+        APIKey +
+        "&units=imperial";
+  
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
+        var forecastDays = [];
+        forecastDays.push(response.list[0]);
+        forecastDays.push(response.list[8]);
+        forecastDays.push(response.list[16]);
+        forecastDays.push(response.list[24]);
+        forecastDays.push(response.list[32]);
+  
+  
+        for (var i = 0; i < forecastDays.length; i += 1) {
+          var forecastContainer = $("<div>").addClass("forecast-day");
+          var dateEl = $("<div>").text(forecastDays[i].dt);
+          var iconEl = $("<div>").text("icon id: " + forecastDays[i].weather[0].icon);
+          var tempEl = $("<div>").text("Temp: " + forecastDays[i].main.temp + " °F");
+          var humidityEl = $("<div>").text("Humidity: " + forecastDays[i].main.humidity + "%");
+          
+          forecastContainer.append(dateEl, iconEl, tempEl, humidityEl);
+          $("#forecast").append(forecastContainer);
+        }
+      });
+    }
+  });
 
-// dump Json for current weather for San Diego
 
 
